e13655837574b2fdc2c0c97f41aa2c6ae0ef40c5
alex <alex.mors@gmx.de>
2024-01-28 10:25:36 +0100

    added help text

--- src/args.rs
+++ src/args.rs
@@ -1,4 +1,4 @@
-use crate::utils;
+use crate::utils::get_latest_aoc_year;
 use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser, Debug)]
@@ -10,38 +10,47 @@ pub struct AocArgs {
 
 #[derive(Subcommand, Debug)]
 pub enum Command {
+    /// Download an Advent of Code puzzle
     Download(DownloadArgs),
+    /// Submit your Advent of Code solution
     Submit(SubmitArgs),
+    /// Set your Advent of Code session cookie
     Cookie { cookie: Option<String> },
 }
 
 #[derive(Args, Debug)]
 pub struct DownloadArgs {
+    /// Optional field for the project path. If empty creates project folders in current working directory
     pub path: Option<String>,
+    /// Specify the Advent of Code Day you want to download. A number between 1 and 25
     #[arg(short, long)]
     pub day: u32,
-    #[arg(short, long)]
+    #[arg(short, long, help=format!("Specify the Advent of Code year you want to download from. A number between 2015 and {}", get_latest_aoc_year()))]
     pub year: u32,
 }
 
 #[derive(Args, Debug)]
 pub struct SubmitArgs {
+    /// Your Advent of Code Solution. Expects a number
     pub solution: u32,
     #[arg(short, long)]
+    /// Specify the Advent of Code Day you want to submit. A number between 1 and 25
     pub day: u32,
-    #[arg(short, long)]
+    #[arg(short, long, help=format!("Specify the Advent of Code year you want to download from. A number between 2015 and {}", get_latest_aoc_year()))]
     pub year: u32,
+    /// Specify the Advent of Code Part you want to submit. A number between 1 and 2
     #[arg(short, long)]
     pub part: u32,
 }
 
 #[derive(Args, Debug)]
 pub struct CookieArgs {
+    /// Optional field to set your session cookie. If empty prints your currently set cookie
     pub cookie: Option<String>,
 }
 
 pub fn check_args(args: &AocArgs) {
-    let aoc_year = utils::get_latest_aoc_year() as u32;
+    let aoc_year = get_latest_aoc_year() as u32;
 
     match &args.command {
         Command::Download(a) => {
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 use std::{env, path::PathBuf};
 use crate::args::DownloadArgs ;
-use tokio;
+use tokio::fs;
 use chrono::prelude::*;
 use scraper::{Html, Selector};
 use anyhow::{Result, anyhow};
@@ -28,27 +28,45 @@ if __name__ == "__main__":
     part_2()
 "#;
 
-async fn create_dirs(input_path: PathBuf, samples_path: PathBuf) -> Result<()> {
-    let input_path_task = tokio::fs::create_dir_all(input_path);
-    let samples_path_task = tokio::fs::create_dir_all(samples_path);
+async fn create_dirs(base_path: &PathBuf) -> Result<()> {
+    let input_path = base_path.join("inputs");
+    let samples_path = base_path.join("samples");
+    let src_path = base_path.join("src");
 
-    let (input_path_res, samples_path_res) = tokio::join!(input_path_task, samples_path_task);
+    let input_path_task = fs::create_dir_all(input_path);
+    let samples_path_task = fs::create_dir_all(samples_path);
+    let src_path_task = fs::create_dir_all(src_path);
 
-    // Handle the results, returning an error if any operation failed.
-    input_path_res.and_then(|_| samples_path_res)?;
+    let results = tokio::join!(src_path_task, input_path_task, samples_path_task);
+
+    results.0?;
+    results.1?;
+    results.2?;
+
     Ok(())
 }
 
-async fn create_files(input_file: PathBuf, samples_file: PathBuf, input: &String,  py_file: PathBuf, py_file_content: &str) -> Result<()> {
-    let input_file_task = tokio::fs::write(input_file, input);
-    let samples_file_task = tokio::fs::write(samples_file, "");
-    let py_file_task = tokio::fs::write(py_file, py_file_content);
+async fn create_files(base_path: &PathBuf, input: &String, args: &DownloadArgs) -> Result<()> {
+    let left_pad = match args.day.to_string().len() {
+        1 => String::from("0"),
+        _ => String::from("")
+    };
+    let day_fmt = format!("day_{}{}", left_pad, args.day);
 
-    let (input_file_res, samples_file_res, py_file_res) = tokio::join!(input_file_task, samples_file_task, py_file_task);
+    let input_file = base_path.join("inputs").join(day_fmt.clone() + ".txt");
+    let samples_file = base_path.join("samples").join(day_fmt.clone() + ".txt");
+    let py_file = base_path.join("src").join(day_fmt.clone() + ".py");
+    let py_file_content = PY_TEMPLATE.replace("REPLACE_DAY", &day_fmt).replace("REPLACE_DAY_NUM", &left_pad);
 
-    input_file_res
-        .and_then(|_| samples_file_res)
-        .and_then(|_| py_file_res)?;
+    let input_file_task = fs::write(input_file, input);
+    let samples_file_task = fs::write(samples_file, "");
+    let py_file_task = fs::write(py_file, py_file_content);
+
+    let results = tokio::join!(input_file_task, samples_file_task, py_file_task);
+
+    results.0?;
+    results.1?;
+    results.2?;
     
     Ok(())
 }
@@ -59,22 +77,8 @@ pub async fn scaffold_project(args: &DownloadArgs, input: &String) -> Result<()>
         None => env::current_dir()?
     };
 
-    let input_path = base_path.join("inputs");
-    let samples_path = base_path.join("samples");
-
-    let left_pad = match args.day.to_string().len() {
-        1 => String::from("0"),
-        _ => String::from("")
-    };
-
-    let day_fmt = format!("day_{}{}", left_pad, args.day);
-    let intput_file = input_path.join(day_fmt.clone() + ".txt");
-    let samples_file = samples_path.join(day_fmt.clone() + ".txt");
-    let py_file = base_path.join(day_fmt.clone() + ".py");
-    let py_file_content = PY_TEMPLATE.replace("REPLACE_DAY", &day_fmt).replace("REPLACE_DAY_NUM", &left_pad);
-
-    create_dirs(input_path, samples_path).await?;
-    create_files(intput_file, samples_file, input, py_file, &py_file_content).await?;
+    create_dirs(&base_path).await?;
+    create_files(&base_path, input, args).await?;
 
     Ok(())
 }
